0403ebe76b9a786b04dda37b359ebfda3d6baee7
X-s <imxs1107@gmail.com>
2016-05-25 00:08:03 +0800

    Use AOSP Battery Service to replace H2OS

--- custom_jar.sh
+++ custom_jar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$1" = "services" ];then
-    cp -rf overlay/services/smali/com/android/server/wallpaper/* $2/smali/com/android/server/wallpaper/
+    cp -rf overlay/services/smali/com/android/server/* $2/smali/com/android/server/
 fi
