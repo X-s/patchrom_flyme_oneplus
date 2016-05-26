2ffa4d4b5099765829dc140ad2321b2a27cff224
X-s <imxs1107@gmail.com>
2016-05-26 19:53:18 +0800

    Use AOSP Ringtone Service to replace H2OS

--- custom_jar.sh
+++ custom_jar.sh
@@ -3,3 +3,7 @@
 if [ "$1" = "services" ];then
     cp -rf overlay/services/smali/com/android/server/* $2/smali/com/android/server/
 fi
+
+if [ "$1" = "framework" ];then
+    cp -rf overlay/framework/smali/android/* $2/smali/android/
+fi
