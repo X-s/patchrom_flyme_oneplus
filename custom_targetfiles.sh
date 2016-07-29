1af7958b2539749c497cc6e048066f8eabef7ce3
X-s <imxs1107@gmail.com>
2016-07-30 04:25:08 +0800

    Rebuild firmware folder

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -2,17 +2,17 @@
 
 TARGET_DIR=out/merged_target_files
 
-cp -f firmware/BTFM.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/emmc_appsboot.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/hyp.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/logo.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/NON-HLOS.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/pmic.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/rpm.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/sbl1.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/sdi.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/static_nvbk.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f firmware/tz.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/BTFM.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/emmc_appsboot.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/hyp.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/logo.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/NON-HLOS.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/pmic.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/rpm.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/sbl1.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/sdi.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/static_nvbk.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/tz.mbn $TARGET_DIR/BOOTABLE_IMAGES/
 
 rm -rf $TARGET_DIR/SYSTEM/framework/hydrongen-framework-res.apk
 
--- releasetools.py
+++ releasetools.py
@@ -3,23 +3,23 @@ import edify_generator
 import os
 
 def InstallBased(info):
-    for filename in os.listdir("firmware"):
-        if not (filename.find('.mbn')==-1 and filename.find('.bin')==-1 and filename.find('static_nvbk')==-1):
-            data=open(os.path.join(os.getcwd(),"firmware",filename)).read()
-            common.ZipWriteStr(info.output_zip, filename, data)
-    extra_img_flash = """ui_print("Writing static_nvbk image...");
-package_extract_file("static_nvbk.bin", "/dev/block/bootdevice/by-name/oem_stanvbk");
-ui_print("Patching firmware images...");
-package_extract_file("sdi.mbn", "/dev/block/bootdevice/by-name/sdi");
-package_extract_file("hyp.mbn", "/dev/block/bootdevice/by-name/hyp");
-package_extract_file("tz.mbn", "/dev/block/bootdevice/by-name/tz");
-package_extract_file("emmc_appsboot.mbn", "/dev/block/bootdevice/by-name/aboot");
-package_extract_file("sbl1.mbn", "/dev/block/bootdevice/by-name/sbl1");
-package_extract_file("pmic.mbn", "/dev/block/bootdevice/by-name/pmic");
-package_extract_file("rpm.mbn", "/dev/block/bootdevice/by-name/rpm");
-package_extract_file("logo.bin", "/dev/block/bootdevice/by-name/LOGO");
-package_extract_file("NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");
-package_extract_file("BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");"""
+    for filename in os.listdir("overlay/firmware-update"):
+        if not (filename.find('.mbn')==-1 and filename.find('.bin')==-1):
+            data=open(os.path.join(os.getcwd(),"overlay/firmware-update",filename)).read()
+            common.ZipWriteStr(info.output_zip, "firmware-update/" + filename, data)
+    extra_img_flash = """ui_print("Writing static_nvbk image");
+package_extract_file("firmware-update/static_nvbk.bin", "/dev/block/bootdevice/by-name/oem_stanvbk");
+ui_print("Patching firmware images");
+package_extract_file("firmware-update/sdi.mbn", "/dev/block/bootdevice/by-name/sdi");
+package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hyp");
+package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");
+package_extract_file("firmware-update/emmc_appsboot.mbn", "/dev/block/bootdevice/by-name/aboot");
+package_extract_file("firmware-update/sbl1.mbn", "/dev/block/bootdevice/by-name/sbl1");
+package_extract_file("firmware-update/pmic.mbn", "/dev/block/bootdevice/by-name/pmic");
+package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpm");
+package_extract_file("firmware-update/logo.bin", "/dev/block/bootdevice/by-name/LOGO");
+package_extract_file("firmware-update/NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");
+package_extract_file("firmware-update/BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");"""
     info.script.AppendExtra(extra_img_flash);
 
 def InstallSuperSU(info):
