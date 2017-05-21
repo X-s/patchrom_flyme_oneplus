99b019e04bfff4825d6592b080e15f3146c374a9
X-s <imxs1107@gmail.com>
2017-05-22 02:48:07 +0800

    add flash firmware-update

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -4,3 +4,13 @@ TARGET_DIR=out/merged_target_files
 
 mv $TARGET_DIR/SYSTEM/framework/flyme-res/flyme-res.apk $TARGET_DIR/SYSTEM/framework/hydrogen-framework-res.apk
 rm -rf $TARGET_DIR/SYSTEM/framework/flyme-res
+
+cp -f overlay/firmware-update/BTFM.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/emmc_appsboot.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/hyp.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/NON-HLOS.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/pmic.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/rpm.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/sbl1.mbn $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/static_nvbk.bin $TARGET_DIR/BOOTABLE_IMAGES/
+cp -f overlay/firmware-update/tz.mbn $TARGET_DIR/BOOTABLE_IMAGES/
---
+++ releasetools.py
@@ -0,0 +1,24 @@
+import common
+import edify_generator
+import os
+
+def InstallBased(info):
+    for filename in os.listdir("overlay/firmware-update"):
+        if not (filename.find('.mbn')==-1 and filename.find('.bin')==-1):
+            data=open(os.path.join(os.getcwd(),"overlay/firmware-update",filename)).read()
+            common.ZipWriteStr(info.output_zip, "firmware-update/" + filename, data)
+    extra_img_flash = """ui_print("Writing static_nvbk image...");
+package_extract_file("firmware-update/static_nvbk.bin", "/dev/block/bootdevice/by-name/oem_stanvbk");
+ui_print("Patching firmware images...");
+package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpm");
+package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");
+package_extract_file("firmware-update/emmc_appsboot.mbn", "/dev/block/bootdevice/by-name/aboot");
+package_extract_file("firmware-update/sbl1.mbn", "/dev/block/bootdevice/by-name/sbl1");
+package_extract_file("firmware-update/pmic.mbn", "/dev/block/bootdevice/by-name/pmic");
+package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hyp");
+package_extract_file("firmware-update/NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");
+package_extract_file("firmware-update/BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");"""
+    info.script.AppendExtra(extra_img_flash);
+
+def FullOTA_InstallEnd(info):
+    InstallBased(info)
