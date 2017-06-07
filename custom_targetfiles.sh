e438146ce9af67a42f3c98ab9c937f1f0418a967
X-s <imxs1107@gmail.com>
2017-06-08 01:12:00 +0800

    Revert "fix Not whitelisted"
    This reverts commit 9c165be783a790756771c09999ad5c453820c4ec.

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -2,8 +2,7 @@
 
 TARGET_DIR=out/merged_target_files
 
-mv $TARGET_DIR/SYSTEM/framework/flyme-res/flyme-res.apk $TARGET_DIR/SYSTEM/framework/hydrogen-framework-res.apk
-rm -rf $TARGET_DIR/SYSTEM/framework/flyme-res
+rm -rf $TARGET_DIR/SYSTEM/framework/hydrogen-framework-res.apk
 
 cp -f overlay/firmware-update/adspso.bin $TARGET_DIR/BOOTABLE_IMAGES/
 cp -f overlay/firmware-update/BTFM.bin $TARGET_DIR/BOOTABLE_IMAGES/
