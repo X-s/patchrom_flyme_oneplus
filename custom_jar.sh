58c16f30111b77388ccb645a6e0f1ca6120c733b
X-s <imxs1107@gmail.com>
2017-06-25 18:01:32 +0800

    move framework smali to smali2, use base smali to replace OnePlus smali

---
+++ custom_jar.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+jarBaseName=$1
+tempSmaliDir=$2
+
+moveDirs=("android/accounts" "android/hardware" "android/printservice" "android/speech")
+
+if [ "$jarBaseName" = "framework" ];then
+    echo ">>>> in custom_jar $jarBaseName to move some smalis"
+    for dir_name in "${!moveDirs[@]}"
+    do
+        mv -v $tempSmaliDir/smali/${moveDirs[$dir_name]} $tempSmaliDir/smali_classes2/${moveDirs[$dir_name]}
+    done
+    echo ">>>> in custom_jar $jarBaseName, use base smali to replace OnePlus smali"
+    rm -rf overlay/temp
+    mkdir -p overlay/temp
+    cp -rf ../base/framework.jar.out/smali_classes2/android/widget/Editor*.smali overlay/temp/
+    cp -rf ../base/framework.jar.out/smali_classes2/android/widget/AbsListView*.smali overlay/temp/
+    idtoname ../base/framework-res/res/values/public.xml overlay/temp/
+    rm -rf $tempSmaliDir/smali_classes2/android/widget/Editor*.smali
+    rm -rf $tempSmaliDir/smali_classes2/android/widget/AbsListView*.smali
+    cp -rf overlay/temp/Editor*.smali $tempSmaliDir/smali_classes2/android/widget/
+    cp -rf overlay/temp/AbsListView*.smali $tempSmaliDir/smali_classes2/android/widget/
+    nametoid framework-res/res/values/public.xml $tempSmaliDir/smali_classes2/android/widget/
+    rm -rf overlay/temp
+fi
