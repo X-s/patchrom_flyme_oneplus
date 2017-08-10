083f17bc436e75be99492465268232b532ca0792
X-s <imxs1107@gmail.com>
2017-08-10 16:38:51 +0800

    Support 3/3T common flash

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -4,20 +4,5 @@ TARGET_DIR=out/merged_target_files
 
 rm -rf $TARGET_DIR/SYSTEM/framework/hydrogen-framework-res.apk
 
-cp -f overlay/firmware-update/adspso.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/BTFM.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/cmnlib.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/cmnlib64.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/devcfg.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/emmc_appsboot.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/hyp.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/keymaster.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/NON-HLOS.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/pmic.elf $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/rpm.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/xbl.elf $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/static_nvbk.bin $TARGET_DIR/BOOTABLE_IMAGES/
-cp -f overlay/firmware-update/tz.mbn $TARGET_DIR/BOOTABLE_IMAGES/
-
 sed -i 's#qemu.sf.lcd_density#persist.sys.density#g' $TARGET_DIR/SYSTEM/lib/libsurfaceflinger.so
 sed -i 's#qemu.sf.lcd_density#persist.sys.density#g' $TARGET_DIR/SYSTEM/lib64/libsurfaceflinger.so
