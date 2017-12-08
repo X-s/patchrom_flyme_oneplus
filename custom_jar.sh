da3852532cd341c308db641030e7cb1904d25a2b
X-s <imxs1107@gmail.com>
2017-12-09 01:31:09 +0800

    Fixed the problem about "Unsigned short value out of range" when make framework

---
+++ custom_jar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+jarBaseName=$1
+tempSmaliDir=$2
+
+moveDirs=("android/accounts" "android/hardware" "android/printservice" "android/speech")
+
+if [ "$jarBaseName" = "framework" ];then
+    echo ">>>> in custom_jar $jarBaseName to move some smalis"
+    for dir_name in "${!moveDirs[@]}"
+    do
+        mv -v $tempSmaliDir/smali/${moveDirs[$dir_name]} $tempSmaliDir/smali_classes2/${moveDirs[$dir_name]}
+    done
+fi
+
